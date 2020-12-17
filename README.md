335217ec86dc8088357f668bd4991db5b04514bb
junior <jpegrande@gmail.com>
2020-12-17 11:45:48 -0500

    macOS initial support

--- README.md
+++ README.md
@@ -1,18 +1,39 @@
 # How to create a windows 10 bootble usb installer on linux
 
 ## Windusb
-This script will install p7zip wimlib and rsync then extract the windows iso to a folder  
-split the fat install.wim into 5 smaller parts that fits within the fat32 limit   
-partition and format the usb drive and copy all the iso files we exracted and splited to it  
-making it a bootble usb driver installer.  
-Supports Ubuntu, Arch Linux, and Fedora based distros.    
+This script will split the fat install.wim into smaller parts that fits within the fat32 limit,
+partition and format the usb drive and copy all the iso files to it,
+making it a bootble usb driver installer.
+Supports Ubuntu, Arch Linux, and Fedora based distros and macOS.
 For more details read dragon788 <a href="https://gist.github.com/dragon788/26921410d8de054366188c5c5435ae01" target="_top">win10_binary_fission.md</a>
-## Usage:
-has to be executed from the same directory as the windows iso  
-``chmod +x windusb``  
-as root  
-``./windusb``  
+Credits to him.
 
+### Linux Usage:
 
-Credits to dragon788 
+In terminal:
 
+   1. Paste the script in the same directory as the windows iso
+   
+   2. Plug in the usb-drive, open your terminal and ``cd`` to the directory contanining the iso and script.
+
+   3. Run `./windusb.sh` enter your password.
+
+   4. select the usb-drive and type ``y`` to start.
+  
+   5. It will take a long time, to umount old 2.0 usb-drives, do not remove it or cancel it before it finishes syncing. 
+
+### macOS Usage:
+
+   1. Plug in the usb-drive
+
+   2. Open Disk utility and format it as ms-dos or exfat, it wont work with any other format.
+
+   3. Paste the script in the same directory as the windows iso and ``cd`` to it.
+
+   5. Run `./windusb.sh` enter your password.
+
+   6. Type in the name you gave you usb-drive when formating eg: ``UNTITLED`` if no name was given.
+
+   7. It will install ``homebrew`` and ``wimlib`` if you don't already have it installed.
+
+   8. It will take a long time, do not cancel or remove the drive before it finishes.
--- windusb.sh
+++ windusb.sh
@@ -2,16 +2,67 @@
 # Autor: Broly
 # License: GNU General Public License v3.0
 # https://www.gnu.org/licenses/gpl-3.0.txt
+installdepend(){
+	if test ! "$(which brew)"; then
+		printf "Installing homebrew...\n"
+		echo | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+		brew update
+		brew install wimlib
+	fi
+}
 
-declare -r cleanup="rm -rf /windUSB/"
-set -e
-[[ "$(whoami)" != "root" ]] && exec sudo -- "$0" "$@"
-
+partextract(){
+	set -e
+	[[ "$(whoami)" != "root" ]] && exec sudo -- "$0" "$@"
+	clear
+	cat << "EOF"
+#######################################
+#  WARNING: THE DRIVE WILL BE ERASED  #
+#######################################
+EOF
+read -r -p 'Enter name of USB media : ' INSTALLER_DEVICE
 clear
 cat << "EOF"
-############################ 
-#    WELCOME TO WINDUSB    # 
-############################ 
+###############################
+#    PARTITIONING THE DRIVE   #
+###############################
+EOF
+diskutil umount /Volumes/"$INSTALLER_DEVICE"
+diskutil eraseVolume ms-dos WINDUSB "$INSTALLER_DEVICE"
+hdiutil attach Win*.iso -mountpoint /Volumes/WINISO
+clear
+rsync -a --progress --no-links --no-perms --no-owner --no-group --exclude sources/install.wim /Volumes/WINISO/ /Volumes/WINDUSB/
+clear
+cat << "EOF"
+###############################
+#    SPLITTING INSTALL.WIM    #
+###############################
+EOF
+printf "this will take a long time\n"
+wimlib-imagex split /Volumes/WINISO/sources/install.wim /Volumes/WINDUSB/sources/install.swm 1024
+hdiutil detach /Volumes/WINISO
+clear
+printf "Installation finished\n"
+exit 1
+}
+
+main(){
+	installdepend "$@"
+	partextract "$@"
+}
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+	main "$@"
+fi
+
+[[ "$(whoami)" != "root" ]] && exec sudo -- "$0" "$@"
+declare -r cleanup="rm -rf /WINBLOWS/"
+set -e
+clear
+cat << "EOF"
+############################
+#    WELCOME TO WINDUSB    #
+############################
 EOF
 sleep 2s
 
@@ -20,9 +71,9 @@ $cleanup
 dependencies(){
 	clear
 	cat << "EOF"
-################################ 
-#    INSTALLING DEPENDENCIES   # 
-################################ 
+################################
+#    INSTALLING DEPENDENCIES   #
+################################
 EOF
 
 sleep 2s
@@ -42,7 +93,7 @@ clear
 
 cat << "EOF"
 ################################################
-#  WARNING: THE SELECTED DRIVE WILL BE ERASED  # 
+#  WARNING: THE SELECTED DRIVE WILL BE ERASED  #
 ################################################
 EOF
 
@@ -61,45 +112,45 @@ fi
 partformat(){
 	clear
 	cat << "EOF"
-############################### 
-#    PARTITIONING THE DRIVE   # 
-############################### 
+###############################
+#    PARTITIONING THE DRIVE   #
+###############################
 EOF
 
 umount "$drive"?* || :
 sgdisk --zap-all "$drive" && partprobe
 sgdisk -e "$drive" --new=0:0: -t 0:0700 && partprobe
 sleep 2s
-mkfs.fat -F32 -n WIND "$drive"1
+mkfs.fat -F32 -n WINDUSB "$drive"1
 mount "$drive"1 /mnt/
-mkdir /windUSB
+mkdir /WINBLOWS
 }
 extract(){
 	clear
 	cat << "EOF"
-############################# 
-#    EXTRACTING ISO FILE    # 
-############################# 
+#############################
+#    EXTRACTING ISO FILE    #
+#############################
 EOF
-7z x Win*.iso -o/windUSB/
+7z x Win*.iso -o/WINBLOWS/
 
 clear
 cat << "EOF"
-############################### 
+###############################
 #    SPLITTING INSTALL.WIM    #
 ###############################
 EOF
 
-wimsplit /windUSB/sources/install.wim /windUSB/sources/install.swm 1000
-rm -rf /windUSB/sources/install.wim
+wimsplit /WINBLOWS/sources/install.wim /WINBLOWS/sources/install.swm 1000
+rm -rf /WINBLOWS/sources/install.wim
 
 clear
 cat << "EOF"
-#################################### 
-#    COPYING FILES TO THE DRIVE    # 
-#################################### 
+####################################
+#    COPYING FILES TO THE DRIVE    #
+####################################
 EOF
-rsync -a --info=progress2 /windUSB/ /mnt/
+rsync -a --info=progress2 /WINBLOWS/ /mnt/
 
 printf "umounting the drive do not remove it or cancel this, it will take a long time!\n"
 umount "$drive"1
@@ -108,7 +159,7 @@ $cleanup
 }
 
 while true; do
-	read -r -p "$(printf %s "Disk ""$drive"" will be erased and wimlib, p7zip, rsync will be installed 
+	read -r -p "$(printf %s "Disk ""$drive"" will be erased and wimlib, p7zip, rsync will be installed
 do you wish to continue [y/n]? ")" yn
 	case $yn in
 		[Yy]* ) dependencies; partformat; extract; break;;
