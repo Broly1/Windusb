987270d8c7f29dd004fdb786b691a74445dce5a6
Broly1 <jpegrande@gmail.com>
2020-12-18 09:51:51 -0800

    Update windusb.sh

--- windusb.sh
+++ windusb.sh
@@ -3,56 +3,63 @@
 # License: GNU General Public License v3.0
 # https://www.gnu.org/licenses/gpl-3.0.txt
 installdepend(){
-	if test ! "$(which brew)"; then
-		printf "Installing homebrew...\n"
-		echo | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-		brew update
-		brew install wimlib
-	fi
+    if test ! "$(which brew)"; then
+        printf "Installing homebrew...\n"
+        echo | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+        echo | brew update
+        echo | brew install wimlib
+        echo | brew install rsync
+    fi
 }
 
 partextract(){
-	set -e
-	[[ "$(whoami)" != "root" ]] && exec sudo -- "$0" "$@"
-	clear
+    set -e
+    [[ "$(whoami)" != "root" ]] && exec sudo -- "$0" "$@"
+    clear
 	cat << "EOF"
 #######################################
 #  WARNING: THE DRIVE WILL BE ERASED  #
 #######################################
 EOF
-read -r -p 'Enter name of USB media : ' INSTALLER_DEVICE
-clear
+    read -r -p 'Enter name of USB media : ' INSTALLER_DEVICE
+    clear
 cat << "EOF"
 ###############################
 #    PARTITIONING THE DRIVE   #
 ###############################
 EOF
-diskutil unmountDisk /Volumes/"$INSTALLER_DEVICE"
-diskutil eraseVolume ms-dos WINDUSB "$INSTALLER_DEVICE"
-hdiutil attach Win*.iso -mountpoint /Volumes/WINISO
-clear
-rsync -a --progress --no-links --no-perms --no-owner --no-group --exclude sources/install.wim /Volumes/WINISO/ /Volumes/WINDUSB/
-clear
+    diskutil unmountDisk /Volumes/"$INSTALLER_DEVICE"
+    diskutil eraseVolume ms-dos WINDUSB "$INSTALLER_DEVICE"
+    hdiutil attach Win*.iso -mountpoint /Volumes/WINISO
+    clear
+cat << "EOF"
+#################################
+#  COPYING FILES TO THE DRIVE   #
+#################################
+EOF
+    printf "\n"
+    rsync -a --info=progress2 --no-links --no-perms --no-owner --no-group --exclude sources/install.wim /Volumes/WINISO/ /Volumes/WINDUSB/
+    clear
 cat << "EOF"
 ###############################
 #    SPLITTING INSTALL.WIM    #
 ###############################
 EOF
-printf "this will take a long time\n"
-wimlib-imagex split /Volumes/WINISO/sources/install.wim /Volumes/WINDUSB/sources/install.swm 1024
-hdiutil detach /Volumes/WINISO
-clear
-printf "Installation finished\n"
-exit 1
+    printf "this will take a long time...\n"
+    wimlib-imagex split /Volumes/WINISO/sources/install.wim /Volumes/WINDUSB/sources/install.swm 1024
+    hdiutil detach /Volumes/WINISO
+    clear
+    printf "Installation finished\n"
+    exit 1
 }
 
 main(){
-	installdepend "$@"
-	partextract "$@"
+    installdepend "$@"
+    partextract "$@"
 }
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
-	main "$@"
+    main "$@"
 fi
 
 [[ "$(whoami)" != "root" ]] && exec sudo -- "$0" "$@"
@@ -69,24 +76,24 @@ sleep 2s
 $cleanup
 
 dependencies(){
-	clear
+    clear
 	cat << "EOF"
 ################################
 #    INSTALLING DEPENDENCIES   #
 ################################
 EOF
-
-sleep 2s
-if [[ -f /etc/debian_version ]]; then
-	apt install -y wimtools p7zip-full rsync
-elif [[ -f /etc/fedora-release ]]; then
-	dnf install -y wimlib-utils p7zip p7zip-plugins rsync
-elif [[ -f /etc/arch-release ]]; then
-	pacman -Syu --noconfirm --needed wimlib p7zip rsync
-else
-	printf "Your distro is not supported!'\n"
-	exit 1
-fi
+    
+    sleep 2s
+    if [[ -f /etc/debian_version ]]; then
+        apt install -y wimtools p7zip-full rsync
+        elif [[ -f /etc/fedora-release ]]; then
+        dnf install -y wimlib-utils p7zip p7zip-plugins rsync
+        elif [[ -f /etc/arch-release ]]; then
+        pacman -Syu --noconfirm --needed wimlib p7zip rsync
+    else
+        printf "Your distro is not supported!'\n"
+        exit 1
+    fi
 }
 
 clear
@@ -101,8 +108,8 @@ EOF
 readarray -t lines < <((lsblk -p -no name,size,MODEL,VENDOR,TRAN | grep "usb"))
 printf "Please select the usb-drive!\n"
 select choice in "${lines[@]}"; do
-	[[ -n $choice ]] || { printf ">>> Invald Selection!\n" >&2; continue; }
-	break
+    [[ -n $choice ]] || { printf ">>> Invald Selection!\n" >&2; continue; }
+    break
 done
 read -r drive _ <<<"$choice"
 if [[ -z "$choice" ]]; then
